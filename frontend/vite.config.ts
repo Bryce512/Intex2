ff0d8ae0802f9eea2cc9f2a88d001bbe4856250a
Bryce512 <bryce.whitworth@gmail.com>
2025-04-05 18:33:47 -0600

    try again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+  build: {
+    outDir: "build", // Output directory
+    assetsDir: "assets", // Where to place assets
+    rollupOptions: {
+      output: {
+        // Ensure JavaScript files have proper extensions
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
+  publicDir: "public",
+});
