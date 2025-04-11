d8c6312d4d66836bf71254076adce4f4a3969675
Bryce512 <bryce.whitworth@gmail.com>
2025-04-10 23:40:23 -0600

    remove publish from github

--- backend/Models/Login.cs
+++
@@ -1,7 +0,0 @@
-namespace Intex2.Models;
-    public class Login
-    {
-        public required string Username { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
